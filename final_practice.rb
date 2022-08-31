78779be3695dfb5371a0af64e382209c63588430
beddings81 <111210465+beddings81@users.noreply.github.com>
2022-08-31 02:42:54 -0500

    Add answers

--- final_practice.rb
+++ final_practice.rb
@@ -1,25 +1,31 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
+def greeting
+  puts "Hello friend"
+end
+# What is the return value of your method? "Hello friend"
+# How many arguments did you pass your method? 0
 
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
-
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
-
+def custom_greeting(name)
+  puts "Hello, #{name}"
+end
+custom_greeting("Braxton")
+# What is the return value of your method? "Hello Braxton"
+# How many arguments did you pass your method? 1
+# What data type was your argument(s)? string
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
+def square(n)
+  n * n
+end
 
-
+square(8)
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
@@ -35,4 +41,3 @@
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
-
