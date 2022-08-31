509a781804a180625022121aa813e555cb110267
beddings81 <111210465+beddings81@users.noreply.github.com>
2022-08-30 23:09:06 -0500

    Add my answers

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,25 @@
 
 # YOU DO the rest:
 
-
+# The include method is called on the string object "Hello world". The argument
+# "Hello" is passed. include must search for argument "Hello" within the string
+# object "Hello world". The return value is "true"
 "Hello World".include?("Hello")
 
-
+# The end_with method is called on the string object "Hello World". The argument
+# "Hello" is passed. end_with must tell us if the string object ends with "hello"
+# The return value is "false"
 "Hello World".end_with?("Hello")
 
-
+# The end_with method is called on the string object "Hello World". The argument
+# "rld" is passed. end_with must tell us if the string object ends with "rld".
+# The return value is "true"
 "Hello World".end_with?("rld")
 
-
+# The even method is called on the integer 12. even will tell us if the integer
+# is an even number. The return value is "true"
 12.even?
 
-
+# The next method is called on the integer 18. next will tell us the successor of
+# the integer. The return value is "19"
 18.next
-
-
